b4992c4cd021547780e0e4c0de3fb09bc199a808
Cory Williams <corywilliams@me.com>
2023-02-02 19:45:25 -0800

    fix nav bar and spacing

--- client/src/components/Homepage/productItem.js
+++ client/src/components/Homepage/productItem.js
@@ -4,8 +4,9 @@ import { pluralize } from "../../utils/helpers"
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-import { Container, Row, Col } from 'react-bootstrap';
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import '../../styles/home.css';
 function ProductItem({item}) {
 //   const [state, dispatch] = useStoreContext();
 
@@ -43,20 +44,26 @@ function ProductItem({item}) {
 //   }
 
   return (
-    <Container>
-      <Row>
-        <Col>
-          <img className="card-img-top img-fluid" src="//placehold.it/500x200" alt="Card image cap"/>
-          <h4 className="card-title">{item.name}</h4>
-          <p className="card-text">${item.price}</p>
-          <button class="btn btn-success flex-shrink-0" type="button">
-          <i className="bi-cart-fill me-1"></i>
-          Add to Cart
-          </button>
-        </Col>
-      </Row>
-    </Container>
+      <div className="col-3 clothe-card">
 
+         {/* <div className="card">
+             <img className="card-img-top img-fluid" src="//placehold.it/500x200" alt="Card image cap"/>
+             <div className="card-block">
+                 <h4 className="card-title">{item.name}</h4>
+                 <p className="card-text">asss</p>
+             <p className="card-text"><small className="text-muted">fasjdkkld</small></p>
+             </div> */}
+ <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{item.name}</Card.Title>
+        <Card.Text>
+          {item.description}{item.sku}
+        </Card.Text>
+        <Button variant="success">Add to Cart</Button>
+      </Card.Body>
+    </Card>
+      </div>
   );
 }
 
