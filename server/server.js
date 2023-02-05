618802cfcab3a867c53b501fed073dde6b40f1e7
teakoVA <nleb641999@gmail.com>
2023-02-05 14:11:50 -0800

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const stripe = require('stripe')('sk_test_51MXFOQIQxvdIT6erA7c1893uceQuSYOzp0Ob6kgAb8qIL7CncaeQ1B2kNt3q69IfNzYh5WqKwoxX1qAcAktzDyno00skRfM7at');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -16,6 +17,47 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Stripe Checkout
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: 'price_1MXFwqIQxvdIT6erZcRcD0KB',
+        quantity: 1,
+      },
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: 'price_1MXFxzIQxvdIT6er7fOJlhCM',
+        quantity: 1,
+      },
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: 'price_1MXG11IQxvdIT6erOs1RS1iW',
+        quantity: 1,
+      },
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: 'price_1MXG05IQxvdIT6er1AJU0tbP',
+        quantity: 1,
+      },
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: 'price_1MXFvYIQxvdIT6erx3zuHrgL',
+        quantity: 1,
+      },
+
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
 
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
